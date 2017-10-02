b2731dc25df19c8e42fab9166235b6e914748b27
“jssbst” <jbustoro16dw@ikzubirimanteo.com>
2017-10-02 09:09:45 +0200

    Initial commit

---
+++ resources/views/helloWorld.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title></title>
+    </head>
+    <body>
+        <h1><a href="agur">Hello World!</a></h1>
+
+        <form action = "get" method = "GET">
+            Name: <input type = "text" name = "name" />
+            Age: <input type = "text" name = "age" />
+            <input type="submit" value="GET" />
+        </form>
+        <br/>
+        <form action = "post" method = "POST">
+            {{ csrf_field() }}
+            Nombre: <input type = "text" name = "nombre" />
+            Edad: <input type = "text" name = "edad" />
+            <input type = "submit" value="POST" />
+        </form>
+    </body>
+</html>
